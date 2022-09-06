f0bb2bd3c1e282a19ac2fad04ff56545bc3a37d9
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2022-09-06 16:53:00 +0530

    life

---
+++ SlashCommands/mod/info.js
@@ -0,0 +1,38 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+  name: "botinfo",
+  description: "Info about the bot",
+  run: async (client, interaction) => {
+    const emb = new MessageEmbed()
+      .setTitle("Bot info")
+      .setDescription("Here is some information about me!")
+      .addFields(
+        {
+          name: "Developer",
+          value: "Arnav Gupta | 9E (<@!908554250945183744>)",
+          inline: true,
+        },
+        {
+          name: "Creation time",
+          value: "<t:1659467762:R>",
+          inline: true,
+        }
+      )
+      .setColor("BLURPLE")
+      .setTimestamp()
+      .setFooter({
+        text: "Special thanks to Aryan and Samridh to allow Arnav to code this <3",
+        iconURL: client.user.avatarURL(),
+      });
+
+    const row = new MessageActionRow().setComponents(
+      new MessageButton()
+        .setLabel("Talk to my daddy")
+        .setStyle("LINK")
+        .setURL("https://discord.com/channels/1004104875522666578/1009473661381652552")
+    );
+    interaction.followUp({ embeds: [emb], components: [row] });
+  },
+};
