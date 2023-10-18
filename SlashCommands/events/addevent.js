791a680ed28a314df64eeba42cb4bab14c07c193
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-10-18 18:07:42 +0530

    Revert "remove events"
    This reverts commit 145d5ba0a8f3e377539e1f513881adba247e2f63.

---
+++ SlashCommands/events/addevent.js
@@ -0,0 +1,53 @@
+const { MessageButton, MessageActionRow } = require("discord.js");
+const { Modal, TextInputComponent } = require("discord-modals");
+
+module.exports = {
+  name: "addevent",
+  np: true,
+  ephemeral: true,
+  description: "MOD ONLY",
+  run: async (client, interaction) => {
+    if (!interaction.member.permissions.has("MANAGE_GUILD")) {
+      return interaction.followUp(
+        "You need `[MANAGE_GUILD]` permission to use this command"
+      );
+    }
+
+    const modal = new Modal()
+      .setCustomId("addevent")
+      .setTitle(`Enter the info below`)
+      .addComponents(
+        new TextInputComponent()
+          .setCustomId("name")
+          .setLabel("Name")
+          .setStyle("SHORT")
+          .setPlaceholder("Please enter the event name")
+          .setRequired(true),
+        new TextInputComponent()
+          .setCustomId("host")
+          .setLabel("Host")
+          .setStyle("SHORT")
+          .setPlaceholder("Please enter the event host")
+          .setRequired(true),
+        new TextInputComponent()
+          .setCustomId("desc")
+          .setLabel("Description")
+          .setStyle("SHORT")
+          .setPlaceholder("Please enter the event description")
+          .setRequired(true),
+        new TextInputComponent()
+          .setCustomId("class")
+          .setLabel("Classes")
+          .setStyle("SHORT")
+          .setPlaceholder("Please enter the event classes eg: (6, 7, 8)")
+          .setRequired(true),
+        new TextInputComponent()
+          .setCustomId("sub")
+          .setLabel("Sub Events")
+          .setStyle("SHORT")
+          .setPlaceholder("Sub events in format - name:desc , name:desc")
+          .setRequired(true)
+      );
+    interaction.showModal(modal);
+  },
+};
